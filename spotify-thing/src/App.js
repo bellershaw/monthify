02288d0eb25c60238f6f0ee65e42cbcff843319d
Braeden <braeden.ellershaw@gmail.com>
2023-08-11 00:50:03 -0700

    changed redirect_uri

--- spotify-thing/src/App.js
+++ spotify-thing/src/App.js
@@ -8,7 +8,7 @@ import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
   const CLIENT_ID = "e818c8d017e44f9ba18d50e657944669"
-  const REDIRECT_URI = "http://localhost:3000"
+  const REDIRECT_URI = "https://bellershaw.github.io/Monthify"
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
 
